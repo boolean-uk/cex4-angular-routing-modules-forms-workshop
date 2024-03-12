e75d4db16f3febc41954a2cd663e885c5e291df0
Carlo M <carlo992@gmail.com>
2024-03-12 09:02:18 -0600

    fixed navlink

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,10 +4,11 @@ import { UsersListComponent } from './users-list/users-list.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { ViewUserComponent } from './view-user/view-user.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [UsersListComponent, CreateUserComponent, ViewUserComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [UsersListComponent, CreateUserComponent, ViewUserComponent],
 })
 export class UsersModule {}
